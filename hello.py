a89ea5d00d8c873deb7500aac9a2905d2af616d3
AayushMathur03 <132337237+AayushMathur03@users.noreply.github.com>
2023-09-30 10:17:56 +0530

    Add files via upload
    day2 completed

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("aayush mathur")
+print(100)
+print('aman')
+# print(0)
+print(" ]abc")
+
+x=100
+y= 3.6
+z = "heythere!!"
+
+print(x,y,z)
+print(type(z))
+
+sum1 = x+y
+mull = x+y
---
+++ task1.py
@@ -0,0 +1,10 @@
+# Q1 input() how does it worksbvv ?
+#stonith tcs go python project
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+result = num1 + num2
+print("The sum of", num1, "and", num2, "is:", result)
